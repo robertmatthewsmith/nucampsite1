bc7a48d7ca62be9194905b4e810ec791814ca360
Robert Smith <robertmatthewsmith1976@gmail.com>
2020-09-19 11:43:14 -0500

    Week 4 workshop2

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#reserveButton").click(function() {
+       $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");     
+    });  
+})
+
